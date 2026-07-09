08623914d8e942e52a31f28b8db52702f6eaa63c
MichaelCorneliusBong <zeexals@student.esaunggul.ac.id>
2026-07-09 23:06:32 +0700

    Pembetulan Minor Terutama Pada Navbar dan Footer

--- src/resources/views/partials/footer.blade.php
+++ src/resources/views/partials/footer.blade.php
@@ -4,8 +4,11 @@
             
             <div>
                 <div class="flex items-center gap-2">
-                    <div class="w-5 h-5 rounded-sm bg-red-600 flex items-center justify-center">
-                        <span class="text-white text-[10px] font-black">G</span>
+                    {{-- New SVG Logo --}}
+                    <div class="w-5 h-5 text-red-600 flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                        </svg>
                     </div>
                     <h2 class="text-md font-bold tracking-tight text-white">
                         Genesys<span class="text-red-500">Meta</span>
--- src/resources/views/partials/navbar.blade.php
+++ src/resources/views/partials/navbar.blade.php
@@ -4,11 +4,11 @@
 
             {{-- Logo --}}
             <a href="{{ route('home') }}" class="flex items-center gap-3 group">
-                <div class="w-9 h-9 rounded bg-red-600 flex items-center justify-center shadow-[0_0_15px_rgba(220,38,38,0.3)] group-hover:bg-red-500 transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
-                    </svg>
-                </div>
+                <div class="w-5 h-5 text-red-600 flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
+                        </svg>
+                    </div>
                 <div class="leading-none">
                     <h1 class="text-white text-lg font-bold tracking-tight transition-colors">
                         Genesys<span class="text-red-500">Meta</span>
